8f50636d6dfd40a642e9de59dcf88d4859adc6b2
smith2eric <ericsmithsonian2@gmail.com>
2022-06-02 14:30:41 +0100

    Completed task twelve

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for num in range(1, 101):
         if num % 3 == 0 and num % 5 == 0:
-            print("Fizzbuzz", end='')
+            print("Fizzbuzz", end=' ')
         elif num % 3 == 0:
             print("Fizz", end=' ')
         elif num % 5 == 0:
