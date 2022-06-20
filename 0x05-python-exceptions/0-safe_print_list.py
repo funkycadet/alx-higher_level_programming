3ff0867da6efd528617dd43dea3695413cf7556a
smith2eric <ericsmithsonian2@gmail.com>
2022-06-20 21:08:23 +0100

    Completed all tasks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-        except(TypeError, NameError, ZeroDivisionError):
+        except(NameError, ZeroDivisionError):
             break
         else:
             count += 1
