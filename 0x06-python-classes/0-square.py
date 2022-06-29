56d14d9ab200d8ee2bee3ea41c595842825dc40e
smith2eric <ericsmithsonian2@gmail.com>
2022-06-29 11:31:13 +0100

    Completed task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def say_hi(self):
+#         print('Hello,', self.name)
+
+
+# Person('swaroop').say_hi()
+class Square:
+    def __init__(self):
+        pass
