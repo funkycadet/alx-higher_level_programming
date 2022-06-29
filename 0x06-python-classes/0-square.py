a9669c8ad107585d8d621b35b380790ab92ad659
smith2eric <ericsmithsonian2@gmail.com>
2022-06-29 12:01:36 +0100

    Completed task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,14 @@
-# #!/usr/bin/python3
-# class Square:
-#     """class Square
-
-#     This is an empty class that defines a square
-
-#     """
-#     pass
 #!/usr/bin/python3
 class Square:
-    """Class Square that defines a square.
+    """class Square
+
+    This is an empty class that defines a square
+
     """
-    pass
+    def main():
+        """module main
+
+        This is an empty module for the class Square
+
+        """
+        pass
