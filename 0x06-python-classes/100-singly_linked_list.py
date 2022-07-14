e5cf353df317c02327a215fb73d3e52f9923b3aa
smith2eric <ericsmithsonian2@gmail.com>
2022-07-14 16:13:58 +0100

    Made changes to sorted_insert method

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -106,6 +106,23 @@ class SinglyLinkedList:
     """
     # __head = None
 
+    def __str__(self):
+        """__str__method
+
+        Method to print singly linked list
+
+        """
+        rtn = ""
+        ptr = self.__head
+
+        while ptr is not None:
+            rtn += str(ptr.data)
+            if ptr.next_node is not None:
+                rtn += "\n"
+            ptr = ptr.next_node
+
+        return rtn
+
     def __init__(self, head=None):
         """__init__ method
 
@@ -120,16 +137,16 @@ class SinglyLinkedList:
         This method is used to sort the elements in a singly linked list
 
         """
-        value = Node(value)
-        if self.__head is None:
-            value.next_node = self.__head
-            self.__head = value
-        elif self.__head.data >= value.data:
-            value.next_node = self.__head
-            self.head = value
+        ptr = self.__head
+
+        while ptr is not None:
+            if ptr.data > value:
+                break
+            ptr_prev = ptr
+            ptr = ptr.next_node
+
+        newNode = Node(value, ptr)
+        if ptr == self.__head:
+            self.__head = newNode
         else:
-            current = self.__head
-            while(current.next_node is not None and
-                    current.next_node.data < value.data):
-                current = current.next_node
-        return value
+            ptr_prev.next_node = newNode
