f067ce055b31afa4c9298df488c7cabd84d86aec
smith2eric <ericsmithsonian2@gmail.com>
2022-07-20 11:10:37 +0100

    [test]Imported external methods

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,7 +9,7 @@ from models.base import Base
 from models.square import Square
 from models.rectangle import Rectangle
 from io import StringIO
-# from unittest import TestCase
+from unittest import TestCase
 from unittest.mock import patch
 
 
