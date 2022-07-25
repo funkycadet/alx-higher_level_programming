803b35c096739a074c7d4c69566d8caa7c8fb27a
smith2eric <ericsmithsonian2@gmail.com>
2022-07-25 10:18:48 +0100

    [update]: Switched variable position in __init__ method

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -78,8 +78,8 @@ class Rectangle:
         Method to initialize width and height argument(s)
 
         """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
