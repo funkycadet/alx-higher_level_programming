1e6111a04baaeae23a43908082042bce423b54b0
smith2eric <ericsmithsonian2@gmail.com>
2022-08-11 23:49:24 +0100

    [feat]: Completed full description task

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Script to print the full description of the table first_table from
+-- hbtn_0c_0 in a MySQL server where database name would be passed as
+-- an argument of the mysql command and DESCRIBE or EXPLAIN can't be used
+SHOW CREATE TABLE first_table;
