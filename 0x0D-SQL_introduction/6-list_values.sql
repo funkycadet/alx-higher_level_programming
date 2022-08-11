7f9930c285a6cf26677c8a9085ee6bdda4c96690
smith2eric <ericsmithsonian2@gmail.com>
2022-08-11 23:55:11 +0100

    [feat]: Completed lit all table task

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- Script to list all rows of the table first_table from database
+-- hbtn_0c_0 in a MySQL server where all fields are printed and\
+-- database name will be passed as an argument of the mysql comand
+SELECT * FROM first_table;
