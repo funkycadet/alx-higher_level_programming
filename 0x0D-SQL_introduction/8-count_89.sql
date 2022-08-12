47c837f371d072c8db9345a20f37dd88b31b9f4d
smith2eric <ericsmithsonian2@gmail.com>
2022-08-12 01:03:41 +0100

    [feat]: Completed count 89 task

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- Script to display number of records with id=89 in table first_table
+-- of database hbtn_0c_0 in a MySQL server where database name would be
+--passed as an argument of the mysql command
+SELECT COUNT(*) AS id FROM first_table WHERE id=89;
+
