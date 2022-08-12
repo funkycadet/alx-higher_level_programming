3d37fc45afdf50e296a9fdd4982b2538afb2d715
smith2eric <ericsmithsonian2@gmail.com>
2022-08-12 02:02:42 +0100

    [feat]: Completed list by best task

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Script to list all records of the second_table of the database hbtn_0c_0
+-- in a MySQL server where its ordered by top score
+SELECT score, name FROM second_table ORDER BY score DESC;
