f4cb21cbee9c3628ef77c19fe015e18d57053d70
smith2eric <ericsmithsonian2@gmail.com>
2022-09-05 21:38:55 +0100

    [feat]: completed task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (!process.argv[2]) {
+  console.log('No argument found');
+} else if (process.argv[2]) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
