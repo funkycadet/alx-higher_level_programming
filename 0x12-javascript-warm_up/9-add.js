8879d9daef26d0d85c66b22b29e9f10acb625f55
smith2eric <ericsmithsonian2@gmail.com>
2022-09-05 23:36:57 +0100

    [update]: fixed function call

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,13 +2,13 @@
 const arg = parseInt(process.argv[2]);
 const arg1 = parseInt(process.argv[3]);
 if (!arg) {
-  console.log('NaN');
+    console.log('NaN');
 } else if (!arg1) {
-  console.log('NaN');
+    console.log('NaN');
+} else {
+    add(arg, arg1);
 }
 
-function add (a, b) {
-  console.log(a + b);
+function add(a, b) {
+    console.log(a + b);
 }
-
-add(arg, arg1);
