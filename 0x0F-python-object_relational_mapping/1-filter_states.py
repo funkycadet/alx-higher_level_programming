c6ec2df7f960091943deb0a3939bad275a1a06d8
smith2eric <ericsmithsonian2@gmail.com>
2022-09-09 14:33:56 +0100

    [feat]: completed task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 1. Filter states module """
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+    query_rows = cur.fetchall()
+    for states in query_rows:
+        print(states)
