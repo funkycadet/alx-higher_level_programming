b9ca57366f8fe9f91d5c52c5f7ee9eb431153a8d
smith2eric <ericsmithsonian2@gmail.com>
2022-09-12 21:04:28 +0100

    [update]: removed whitespace on empty line

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,7 +5,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ rectangle class which inherits from BaseGeometry """
-    
+
     def __init__(self, width, height):
         """ __init__ method to initialize instances """
         self.integer_validator("width", width)
