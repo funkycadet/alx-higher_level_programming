e8821944127adf9304ef60f2d62752c431ce365d
smith2eric <ericsmithsonian2@gmail.com>
2022-09-23 15:40:30 +0100

    [update]: added utf-8 for decoding str

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     try:
         with request.urlopen(url) as response:
-            print(response)
+            print(response.read().decode('utf-8'))
     except error.HTTPError as e:
         print(f"Error code: {e.code}")
