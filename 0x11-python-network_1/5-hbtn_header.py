1fbe04d95a41008ec0e42c0cd7b7eaa23a073f26
smith2eric <ericsmithsonian2@gmail.com>
2022-09-23 16:12:53 +0100

    [update]: added try and except clause

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         url = sys.argv[1]
         req = requests.get(url)
         print(req.headers['X-Request-Id'])
-    except False:
+    except requests.RequestException:
         pass
