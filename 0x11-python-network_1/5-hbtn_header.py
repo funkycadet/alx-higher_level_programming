2e8446d817e695469ec4ae4f16b49849203745d8
smith2eric <ericsmithsonian2@gmail.com>
2022-09-23 15:48:41 +0100

    [feat]: completed task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" module to display variable value in response header from a given URL """
+import requests
+import sys
+
+url = sys.argv[1]
+req = requests.get(url)
+print(req.headers['X-Request-Id'])
