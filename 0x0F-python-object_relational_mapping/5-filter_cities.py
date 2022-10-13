dce5bb77a104169524ed8feb11174e718aa53217
smith2eric <ericsmithsonian2@gmail.com>
2022-10-13 12:03:37 +0100

    [fix]: resolved errors

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-""" List cities of state passed as an argument from database """
-import MySQLdb
+"""
+List all cities of a state
+"""
 import sys
+import MySQLdb
 
 if __name__ == '__main__':
-    conn = MySQLdb.connect(
-        user=sys.argv[1], passwd=sys.argv[2],
-        db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    cur = conn.cursor()
+    cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
     FROM cities JOIN states ON cities.state_id = states.id \
     WHERE states.name = '{}';".format(sys.argv[4]))
-    query_rows = cur.fetchall()
-    for state in query_rows:
-        print(", ".join([state[1] for state in query_rows]))
+    states = cur.fetchall()
+
+    print(", ".join([state[1] for state in states]))
